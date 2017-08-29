71e57a08a7c77ffa680b3f106978fc31c9ddb520
Anupam Majhi <majhi.anupam@gmail.com>
2017-08-29 06:38:03 +0530

    Line update

--- UberAssignment.R
+++ UberAssignment.R
@@ -11,3 +11,11 @@ UberData <- read.csv("Uber Request Data.csv")
 
 # Check duplicate values
 
+  sum(duplicated(UberData$Request.id)) #No Duplicates
+
+# Checking and validating missing values
+
+  # All rows should have Request Time Stamp, Pickup point and Request ID
+  sum(is.na(UberData$Request.timestamp)) #No missing data
+  sum(is.na(UberData$Request.id)) #No missing data
+  sum(is.na(UberData$Pickup.point)) #No missing data
