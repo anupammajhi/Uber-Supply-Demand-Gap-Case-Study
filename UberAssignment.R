c1b3874d346fedd434509c4e3fcf741e1af86467
Anupam Majhi <majhi.anupam@gmail.com>
2017-08-29 06:58:46 +0530

    Changes in code

--- UberAssignment.R
+++ UberAssignment.R
@@ -19,3 +19,11 @@ UberData <- read.csv("Uber Request Data.csv")
   sum(is.na(UberData$Request.timestamp)) #No missing data
   sum(is.na(UberData$Request.id)) #No missing data
   sum(is.na(UberData$Pickup.point)) #No missing data
+  
+  # Check Similar values
+  levels(UberData$Status) #All distinct values, unambiguous, No need to lower case
+  levels(UberData$Pickup.point) #All distinct values, unambiguous, No need to lower case
+
+  # Request.id and Pickup.point should not have any missing Values
+  sum(is.na(UberData$Request.id)) #No missing values
+  sum(is.na(UberData$Pickup.point)) #No missing values
