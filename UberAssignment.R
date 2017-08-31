81f0e6b59116b8172c9039f88b2f9082d89ef619
Anupam Majhi <majhi.anupam@gmail.com>
2017-08-31 08:23:42 +0530

    Update and commit

--- UberAssignment.R
+++ UberAssignment.R
@@ -80,3 +80,4 @@ UberData <- read.csv("Uber Request Data.csv")
     
     # Identifying rows with Date format %d-%m-%Y %H:%M:%S using regex
     hyphenDates_Drop <- grep(pattern = '^[0-9]{1,2}-[0-9]{1,2}-[0-9]{4} [0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2}$',x=UberData$Drop.timestamp)
+    
