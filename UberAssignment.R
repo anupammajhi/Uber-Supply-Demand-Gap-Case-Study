882b628b4d4f48f67816ec888829efa277d32a16
Anupam Majhi <majhi.anupam@gmail.com>
2017-09-04 08:53:47 +0530

    Line update

--- UberAssignment.R
+++ UberAssignment.R
@@ -174,3 +174,4 @@ UberData <- read.csv("Uber Request Data.csv")
              y='Number of Requests', 
              title='Overall Response to Demand')
       #Explanation : bar chart is used, labs has been used to change graph labels, geom_text for adding text to graph with 
+      # vjust for adjusting text position, ..count.. identity to specify that the frequency itself is used and size for text size
