bca44d59927da321b579d469aa60dcd2768220b7
Anupam Majhi <majhi.anupam@gmail.com>
2017-09-13 06:25:00 +0530

    Commit to change

--- UberAssignment.R
+++ UberAssignment.R
@@ -439,3 +439,11 @@ UberData <- read.csv("Uber Request Data.csv")
       UberData[which(UberData$Status == 'No Cars Available' & UberData$Pickup.point == 'Airport'),] %>%
         group_by(HourOfTheDay) %>%
         summarise(NonAvailability = length(HourOfTheDay)) %>%
+        summarise(Average_Non_Availabilty = mean(NonAvailability,na.rm=T))
+      
+      #From City
+      UberData[which(UberData$Status == 'No Cars Available' & UberData$Pickup.point == 'City'),] %>%
+        group_by(HourOfTheDay) %>%
+        summarise(NonAvailability = length(HourOfTheDay)) %>%
+        summarise(Average_Non_Availabilty = mean(NonAvailability,na.rm=T))
+
