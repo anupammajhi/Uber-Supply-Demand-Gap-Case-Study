76ce45ab72c9364d1c7bfa2b53eb408be85d9ee3
Anupam Majhi <majhi.anupam@gmail.com>
2017-09-15 09:05:01 +0530

    Update and commit

--- UberAssignment.R
+++ UberAssignment.R
@@ -446,3 +446,4 @@ UberData <- read.csv("Uber Request Data.csv")
         group_by(HourOfTheDay) %>%
         summarise(NonAvailability = length(HourOfTheDay)) %>%
         summarise(Average_Non_Availabilty = mean(NonAvailability,na.rm=T))
+
